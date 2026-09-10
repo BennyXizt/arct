d96cbddf508d2d02d37726385ef37d0fed032fa7
BennyXizt <64214352+BennyXizt@users.noreply.github.com>
2026-09-10 14:14:22 +0200

    [UPDATE]: Upd

--- externe/scripts/gallery/utils/utils.ts
+++ externe/scripts/gallery/utils/utils.ts
@@ -80,9 +80,12 @@ export function moveImage(gallery: GalleryElementInterface, lightBox: HTMLElemen
     }, { once: true })
 
     requestAnimationFrame(() => {
-        nextFigureHTML.classList.remove('next-right')
-        nextFigureHTML.classList.remove('next-left')
         nextFigureHTML.classList.add('current')
+
+        requestAnimationFrame(() => {
+            nextFigureHTML.classList.remove('next-right')
+            nextFigureHTML.classList.remove('next-left')
+        })
     })
     
     if(counterHTML)
